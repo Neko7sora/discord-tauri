ac8e7e7794b62e5a67087b5f569ad2bab054e7e3
c0b23039a5 <75793267+Neko7sora@users.noreply.github.com>
2026-03-23 21:58:46 +0900

    ネイティブのタイトルバーを無効に

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -237,6 +237,7 @@ fn main() {
 
             WebviewWindowBuilder::new(app, "main", WebviewUrl::External(discord_url))
                 .title(APP_TITLE)
+                .decorations(false)
                 .inner_size(1320.0, 860.0)
                 .min_inner_size(920.0, 640.0)
                 .resizable(true)
